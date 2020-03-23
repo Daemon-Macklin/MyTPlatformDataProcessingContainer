0cd39b8e876f7e611beaf7177fcb0a1a90084871
Daemon-Macklin <daemon.macklin@outlook.com>
2020-03-23 01:30:22 +0000

    Adding mysqldb.py file

--- databaseLibs/mysqldb.py
+++ databaseLibs/mysqldb.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import datetime
 import loggerHelper
 import json
@@ -5,7 +6,35 @@ import json
 client = None
 
 def connect():
-    pass
+    global client
+    try:
+        client = mysql.connector.connect(host="mysqldb", database="MyTData")
+    except:
+        loggerHelper.getLogger().info("Error Connecting to MySQL")
+
+    if client is None:
+        loggerHelper.getLogger().info("Error Connecting to MySQL")
+
 
 def writeData(body):
-    pass
+    global client
+    data=body["data"]
+    date=datetime.datetime.now()
+    varnames = ""
+    formatting = ""
+    vals = ()
+    for key, value in data.items():
+        varnames = varnames + key + ","
+        formatting = formatting + "%s,"
+        vals = vals + (value,)
+    varnames = varnames[:-1]
+    formatting = formatting[:-1]
+    loggerHelper.getLogger().info(varnames)
+    loggerHelper.getLogger().info(vals)
+
+    command = "INSERT INTO SensorData (" + varnames + ") VALUES (" + formatting + ")"
+    loggerHelper.getLogger().info(command)
+
+    client.cursor.execute(command, vals)
+
+    client.commit()
